a07378cc627122c8ec12cc2c4311f8e2db348805
thulana <thulanakannangara@gmail.com>
2017-07-20 12:25:37 +0530

    add docker file and test cases

---
+++ tests/unit/testDockerContainerCest.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class testDockerContainerCest
+{
+    public function _before(UnitTester $I)
+    {
+    }
+
+    public function _after(UnitTester $I)
+    {
+    }
+
+    // tests
+    public function testApacheService(UnitTester $I)
+    {
+		
+		$I->am('Docker Tester');
+		$I->wantTo('verify apache in the container');
+		$I->runShellCommand('docker exec apache-container service apache2 status');
+		$I->seeInShellOutput('apache2 is running');
+    }
+     public function testMySQLService(UnitTester $I)
+    {
+		
+		$I->am('Docker Tester');
+		$I->wantTo('verify apache in the container');
+		$I->runShellCommand('docker exec apache-container service mysql status');
+		$I->seeInShellOutput('is running');
+    }
+}
